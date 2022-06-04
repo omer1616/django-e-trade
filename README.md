f9a0580898dd6b40f76d4644c225b34fb1183d7d
omer1616 <omerinci415@gmail.com>
2022-06-04 16:08:03 +0300

    addded project

---
+++ README.md
@@ -0,0 +1,1 @@
+# django-e-trade
---
+++ page/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    context = dict()
+
+    return render(request,  'home/index.html')
+
+
