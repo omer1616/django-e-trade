854987d4d619138f71cc610a3b8fffe8dcc6a507
omer1616 <omerinci415@gmail.com>
2022-06-05 11:02:28 +0300

    create migrations

--- page/models.py
+++ page/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+STATUS = [
+    ('draft',  'Taslak'),
+    ('published', 'Yayinlandi'),
+    ('deleted', 'Silindi'),
+]
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    # slug = models.SlugField
+    content = models.TextField()
+    cover_image = models.ImageField(upload_to='page')
+    status = models.CharField(
+        default="draft",
+        choices=STATUS,
+        max_length=10,
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
