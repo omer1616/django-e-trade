7d9f3361b18e61b250ffbec792dc7c52faebdf80
omer1616 <omerinci415@gmail.com>
2022-06-07 22:41:15 +0300

    added courusel

--- page/admin.py
+++ page/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Page
+from .models import Page, Carousel
+
+
 # Register your models here.
 
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'content', 'status', 'create_at']
+    prepopulated_fields = {"slug": ('title',)}
+    list_filter = ['status', 'create_at']
+    # list_editable = ['status', 'title']
+
+
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'status', 'create_at']
+    list_filter = ['status', 'create_at']
+
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(Carousel, CarouselAdmin)
--- page/models.py
+++ page/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+DEFAULT_STATUS = "draft"
 STATUS = [
     ('draft',  'Taslak'),
     ('published', 'Yayinlandi'),
@@ -11,13 +11,29 @@ STATUS = [
 
 class Page(models.Model):
     title = models.CharField(max_length=200)
-    # slug = models.SlugField
+    slug = models.SlugField(max_length=200, null=True)
     content = models.TextField()
-    cover_image = models.ImageField(upload_to='page')
+    cover_image = models.ImageField(upload_to='page',  null=True,  blank=True)
     status = models.CharField(
-        default="draft",
+        default=DEFAULT_STATUS,
         choices=STATUS,
         max_length=10,
     )
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+
+class Carousel(models.Model):
+    title =  models.CharField(max_length=200,  blank=True, null=True)
+    cover_image = models.ImageField(
+        upload_to= "carousel",
+        null=True,
+        blank= True,
+    )
+    status =  models.CharField(
+        default =  DEFAULT_STATUS,
+        choices= STATUS,
+        max_length=10,
+    )
+    create_at =  models.DateTimeField(auto_now_add=True)
+    uptate_at =  models.DateTimeField(auto_now=  True)
